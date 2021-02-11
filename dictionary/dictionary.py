39e75bf8c3405b54423d614e8cbf71c07b69dbbf
LordCod3r <64400282+Anonymous10m@users.noreply.github.com>
2021-02-11 15:44:29 +0530

    Update dictionary.py

--- dictionary/dictionary.py
+++ dictionary/dictionary.py
@@ -7,22 +7,19 @@ def meaning(w):
     w = w.lower()
     if w in data:
         return data[w]
-    elif w.title in data:
-        return data[w.title()]
-    elif w.upper in data:
-        return data[w.upper()]        
-    elif len(get_close_matches(w,data.keys())) > 0:
-        yn = input("Do you mean %s instead? Enter y if yes else enter n: " % get_close_matches(w, data.keys()) [0])
-        if yn == 'y' or 'Y':
-             return data[get_close_matches(w, data.keys())[0]]
+    elif len(get_close_matches(w, data.keys())) > 0:
+        yn = input("Did you mean %s instead? Enter Y if yes, or N if no: " % get_close_matches(w, data.keys())[0])
+        if yn == "Y":
+            return data[get_close_matches(w, data.keys())[0]]
+        elif yn == "N":
+            return "Word not found."
         else:
-            return "Word not found"
+            return "Word not found."
     else:
-        return "Word not found."    
+        return "Word not found."
 
-word = input("Enter word: ")    
-output = (meaning(word))
-
+word = input("Enter word: ")
+output = meaning(word)
 if type(output) == list:
     for item in output:
         print(item)
